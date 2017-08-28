c440ffb78a684eecdafab2530fb0311eee0971ca
Maria Clara Santana <claarasantana@gmail.com>
2017-08-28 10:01:36 -0300

    🔧  setup basic react-intl

--- src/index.js
+++ src/index.js
@@ -2,15 +2,29 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { addLocaleData, IntlProvider } from 'react-intl'
+import en from 'react-intl/locale-data/en'
+import pt from 'react-intl/locale-data/pt'
+import { flattenMessages } from './utils'
+import messages from './messages'
 import App from './components/App'
 import reducer from './reducers'
 import 'semantic-ui-css/semantic.min.css'
 
+addLocaleData([...en, ...pt])
+let locale = 
+  (navigator.languages && navigator.languages[0])
+  || navigator.language
+  || navigator.userLanguage
+  || 'en-US'
+
 const store = createStore(reducer)
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </IntlProvider>,
   document.getElementById('root')
 )
---
+++ src/messages.js
@@ -0,0 +1,12 @@
+export default {
+  'en-US': {
+    header: 'To Do List',
+    inputPlaceholder: 'Insert here your to do',
+    trigger: 'SAVE'
+  },
+  'pt-BR': {
+    header: 'Lista de Afazeres',
+    inputPlaceholder: 'Insira aqui o seu afazer',
+    trigger: 'SALVAR'
+  }
+}
