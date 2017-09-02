8964ab27d120832820fd883804af7054d86a43e3
Maria Clara Santana <claarasantana@gmail.com>
2017-09-01 22:54:27 -0300

    🎨  style add todo button

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -4,39 +4,47 @@ import { connect } from 'react-redux'
 import { addTodo } from '../actions'
 import { FormattedMessage, intlShape, injectIntl } from 'react-intl'
 import { Form } from 'semantic-ui-react'
+import styled from 'styled-components'
+
+const AddButton = styled(Form.Button) `
+  button {
+    color: white !important;
+    background: black !important;
+  }
+`
 
 export class AddTodo extends Component {
   constructor(props) {
     super(props)
     this.state = { todo: '' }
-    this.handleChange = this.handleChange.bind(this); 
-    this.handleAddTodo = this.handleAddTodo.bind(this);        
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAddTodo = this.handleAddTodo.bind(this);
   }
 
   handleChange(e) { this.setState({ [e.target.id]: e.target.value }) }
 
   handleAddTodo(e) {
     if (this.state.todo.length <= 0) return
-    
-    let date = new Date().toLocaleDateString()   
+
+    let date = new Date().toLocaleDateString()
     this.props.addTodo(this.state.todo, date)
     this.setState({ todo: '' })
   }
-  
+
   render() {
     const intl = this.props.intl
-    
+
     const placeholder = intl.formatMessage({
       id: "inputPlaceholder"
     })
 
-    return(
+    return (
       <Form>
         <Form.TextArea id='todo' value={this.state.todo} onChange={this.handleChange}
-        autoHeight placeholder={placeholder} />
-        <Form.Button type='submit' onClick={this.handleAddTodo}>
+          autoHeight placeholder={placeholder} />
+        <AddButton type='submit' onClick={this.handleAddTodo}>
           <FormattedMessage id="trigger" />
-        </Form.Button>
+        </AddButton>
       </Form>
     )
   }
@@ -45,7 +53,7 @@ AddTodo.propTypes = {
   intl: intlShape.isRequired
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ 
+const mapDispatchToProps = dispatch => bindActionCreators({
   addTodo: addTodo
 }, dispatch)
 
